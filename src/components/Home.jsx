e0fffd21ad8538089dfbf8db8a07217eb98fa250
Lopamudra Mallick <88570040+Lopamudra00@users.noreply.github.com>
2024-01-31 23:00:00 +0530

    toggle added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,85 +6,104 @@ const Home = () => {
     const [apiData, setApiData] = useState(null);
     const [search, setSearch] = useState('');
     const [err, setErr] = useState('');
+    const [isCelsius, setIsCelsius] = useState(true);
+    const [temp, setTemp] = useState("");
+    const [maxTemp, setMaxTemp] = useState(null);
+    const [minTemp, setMinTemp] = useState(null);
+    const [dark, setDark] = useState(false);
     useEffect(() => {
         const fetchData = async () => {
             const res = await fetch(Weather_API + search + API_KEY
             )
             const json = await res.json();
-            if (res.ok)
-                setApiData(json)
-            if (json.cod === '404' || json.cod === '400')
-                setErr(json.message)
-            else
-                setErr('')
+            if (res?.ok) {
+                setApiData(json);
+                setTemp(json?.main?.temp);
+                setMaxTemp(json?.main?.temp_max);
+                setMinTemp(json?.main?.temp_min);
+            }
+            if (json.cod === "404" || json.cod === "400") setErr(json.message);
+            else setErr("");
         }
         fetchData()
     }, [search])
+    const toggleUnits = () => {
+        setIsCelsius(!isCelsius);
+    };
+    const toggleTheme = () => {
+        setDark(!dark);
+    }
 
     return (
-        <div className="p-2 bg-[#040D28]">
-            <div className="flex justify-evenly shadow-xl">
+        <div className={`p-2 ${dark ? "bg-[#040D28]" : "bg-[#2E4874]"}`}>
+            <div className={`flex justify-evenly shadow-xl rounded-2xl ${dark ? "bg-[#2E4874]" : "bg-[#947FAF]"} `}>
                 {/* navbar */}
                 <div className="mr-80 font-bold p-2 text-white text-4xl">Weather Forecast</div>
+                <button onClick={toggleTheme} className="text-white">Switch Theme</button>
                 <div className="ml-80">
                     <input
-                        className="p-1 border rounded-lg" type="text"
+                        className="p-1 m-1 border rounded-lg" type="text"
                         placeholder="search city"
                         value={search}
                         onChange={(e) => setSearch(e.target.value)}
                     />
-                    <div className="text-white">{err}</div>
+                    <div className="text-white ml-2">{err}</div>
                 </div>
 
             </div>
             <div className="mt-2 relative">
-                <img className="w-full h-80 rounded-b-3xl shadow-xl" src={BG_IMG} alt="bg-img" />
-                <div className="absolute ">
-                    {/* Your content inside the overlay div */}
-                    <p className="text-white">Overlay Content</p>
+                <img className="w-full h-80 rounded-3xl shadow-xl object-cover" src={BG_IMG} alt="bg-img" />
+                <div className="absolute ml-64 -mt-10">
+                    <button onClick={toggleUnits} className="text-white border rounded-lg p-2 shadow-sm shadow-gray-200">Units</button>
                 </div>
             </div>
             <div className="flex">
                 <div className="h-80 w-2/5 mx-2 my-4">
                     <div className="flex flex-wrap gap-5 my-4">
-                        <div className="text-white h-32 w-44 bg-slate-800 rounded-xl justify-center items-center">
+                        <div className="text-white h-32 w-44 bg-slate-800 rounded-xl justify-center items-center shadow-md shadow-gray-200">
                             <p className="text-center mt-8">
                                 Temperature
                                 <br />
-                                {apiData && (apiData.main.temp - 273.15).toFixed(1) + "°C"}
+                                {isCelsius
+                                    ? temp - 273.15 + " °C"
+                                    : (temp - 273.15) * (9 / 5) + 32 + " °F"}
 
                             </p>
                         </div>
-                        <div className="text-white h-32 w-44 bg-slate-800 rounded-xl justify-center">
+                        <div className="text-white h-32 w-44 bg-slate-800 rounded-xl justify-center shadow-md shadow-gray-200">
                             <p className="text-center mt-8">
                                 Minimun temp
                                 <br />
-                                {apiData && (apiData.main.temp_min - 273.15).toFixed(1) + "°C"}
+                                {isCelsius
+                                    ? minTemp - 273.15 + " °C"
+                                    : (minTemp - 273.15) * (9 / 5) + 32 + " °F"}
 
                             </p>
                         </div>
-                        <div className="text-white h-32 w-44 bg-slate-800 rounded-xl justify-center ">
+                        <div className="text-white h-32 w-44 bg-slate-800 rounded-xl justify-center shadow-md shadow-gray-200">
                             <p className="text-center mt-8">
                                 Maxi temp
                                 <br />
-                                {apiData && (apiData.main.temp_max - 273.15).toFixed(1) + " °C"}
+                                {isCelsius
+                                    ? maxTemp - 273.15 + " °C"
+                                    : (maxTemp - 273.15) * (9 / 5) + 32 + " °F"}
                             </p>
                         </div>
-                        <div className="text-white h-32 w-44 bg-slate-800 rounded-xl justify-center items-center">
+                        <div className="text-white h-32 w-44 bg-slate-800 rounded-xl justify-center items-center shadow-md shadow-gray-200">
                             <p className="text-center mt-8">
                                 Humidity
                                 <br />
                                 {apiData && apiData.main.humidity + "%"}
                             </p>
                         </div>
-                        <div className="text-white h-32 w-44 bg-slate-800 rounded-xl justify-center items-center">
+                        <div className="text-white h-32 w-44 bg-slate-800 rounded-xl justify-center items-center shadow-md shadow-gray-200">
                             <p className="text-center mt-8">
                                 wind speed  <br />{apiData && apiData.wind.speed + " m/s"}
 
 
                             </p>
                         </div>
-                        <div className="text-white h-32 w-44 bg-slate-800 rounded-xl justify-center items-center">
+                        <div className="text-white h-32 w-44 bg-slate-800 rounded-xl justify-center items-center shadow-md shadow-gray-200">
                             <p className="text-center mt-8">
                                 wind direction <br /> {apiData && apiData.wind.deg + "° rel to N"}
 
@@ -93,7 +112,7 @@ const Home = () => {
 
                     </div>
                 </div>
-                <div className="h-[275px] w-3/5 mx-5 mt-8 bg-slate-800 rounded-xl justify-center items-center">
+                <div className="h-[275px] w-3/5 mx-5 mt-8 bg-slate-800 rounded-xl justify-center items-center shadow-md shadow-gray-200">
                     <Chart search={search} />
                 </div>
 
